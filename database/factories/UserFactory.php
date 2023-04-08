743d09c7051542ea0bb1444c1b5377908a14df22
Denis Vykhodtsev <d.vyhodcev@softmg.ru>
2023-04-08 14:31:22 +0000

    seeder users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,21 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'nickname' => $this->faker->word(),
+            'firstname' => $this->faker->firstName(),
+            'lastname' => $this->faker->lastName(),
+            'avatar' => $this->faker->imageUrl(640, 480, 'animals', true),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'telephone' => $this->faker->ean13(),
+            'description' => $this->faker->paragraph(),
+            'location' => $this->faker->country(),
+            'active' => $this->faker->boolean(),
+            'admin' => $this->faker->boolean(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'updated_at'=>now(),
+            'created_at'=>now(),
         ];
     }
 
