946b02831b9aeaef24a9a845e7e175d5a5116200
Denis Vykhodtsev <d.vyhodcev@softmg.ru>
2023-10-28 15:15:12 +0000

    add telescope

--- config/database.php
+++ config/database.php
@@ -130,7 +130,7 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => 'null',
+            'password' => '',
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
@@ -138,7 +138,7 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'), 
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => 'null',
+            'password' => '',
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
